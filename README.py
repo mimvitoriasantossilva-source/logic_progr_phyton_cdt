576bfb23ca9903f668c50b902c4fe60c7803b2c2
mimvitoriasantossilva-source <259363047+mimvitoriasantossilva-source@users.noreply.github.com>
2026-07-20 09:46:04 -0300

    update

---
+++ README.py
@@ -0,0 +1,50 @@
+'''
+Nesse modulo 05, a gente aprendeu a Logica de Programaçao para iniciantes, a gente aprendeu:
+Algoritmos: Sequência lógica para resolução de problemas.
+Estruturas de Dados: Uso estratégico de Listas [], Tuplas () e Dicionários {}.
+Controle de Fluxo: Tomada de decisão com if/else e repetições com while/for.
+Tratamento de Erros: Garantir que o programa não pare por entradas inválidas.
+
+# Projetos de destaque: Hamburgueria - Ovo - Bolo - Batata - Pneu - Cabelo.
+Como aplicação principal dos fundamentos, desenvolvemos um simulador completo dos projetos no destaque
+
+# ✨Funcionalidades principais
+Estes scripts foram desenvolvidos em **Python** e projetados para serem executados em ambiente de 
+desenvolvimento como o VS Code. Trata-se de guias interativos em linha de comando (CLI) que orientam o 
+usuário passo a passo na execução de tarefas do dia a dia (receitas culinárias, cuidados pessoais e 
+manutenção de veículos).
+
+# Principais Funcionalidades Unificadas:
+
+* Instruções Detalhadas:** Cada script apresenta um guia completo descrevendo o processo do início ao fim.
+* Passo a Passo Estruturado:** Uma função dedicada imprime no terminal do VS Code um roteiro numerado com 
+10 passos essenciais para a realização da tarefa.
+* Personalização de Resultados:** Através de estruturas condicionais (`if/else`), o resultado final é 
+adaptado com base na preferência selecionada pelo usuário.
+* Menu Interativo (CLI):** Apresenta opções simples via console, recebe a entrada do usuário (`input()`) e 
+exibe o status final do processo concluído.
+
+📰Estrutura dos repositórios
+batata_frita.py: Exemplo prático de guia interativo para preparar batata frita.
+bolo.py: Exemplo prático de guia interativo para fazer um bolo.
+cabelo.py: Exemplo prático de guia interativo para passos de lavagem de cabelo.
+hamburguer.py: Exemplo prático de guia interativo para montar um hambúrguer.
+ovo.py: Exemplo prático de guia interativo para o preparo de ovo frito.
+trocar_pneu.py: Exemplo prático de guia interativo para troca de pneu com segurança.
+
+# 🛠️Tecnologias Utilizadas
+Python 3.x: Linguagem principal.
+Random: Biblioteca para sorteios e sugestões.
+PyInstaller: Utilizado para converter o script .py em um executável (.exe).
+
+🚀 Como Começar
+Pré-requisitos
+Ter o Python instalado.
+Instalação e Execução
+Clone o repositório:
+git clone [LINK_DO_SEU_REPOSITORIO]
+Entre na pasta:
+cd fundamentos-logica-python
+Execute o sistema da hamburgueria:
+python hamburgueria.py
+'''
